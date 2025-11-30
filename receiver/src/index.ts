0d7236de6b1fe3bead5efa02eae13ee714595657
Bassel Sallam <Bassela.sallam@gmail.com>
2025-11-30 19:09:50 +0200

    test

---
+++ receiver/src/index.ts
@@ -0,0 +1,51 @@
+import { connect } from "nats";
+const SUBJECT = "orders";
+import activityService from "models/activity/activity.service";
+import express from "express";
+import { connectDB } from "@config/DB.config";
+import "dotenv/config";
+const app = express();
+
+
+
+
+async function runSubscriber() {
+  let nc;
+  try {
+    nc = await connect({ servers: process.env.NATS_URL });
+    console.log(`connect successfully to ${nc.getServer()}`);
+    const subscription = nc.subscribe(SUBJECT);
+
+    console.log(`i lesten to ${SUBJECT}`);
+    for await (const msg of subscription) {
+      const data = msg.data.toString();
+      try {
+        const order = JSON.parse(data);
+        console.log(`new order has been received`);
+        console.log(order);
+        activityService.createOne(order);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (nc) {
+      await nc.close();
+    }
+  }
+}
+
+runSubscriber();
+
+app.get("/log", async (req, res, next) => {
+  const data = await activityService.getAll();
+  console.log(data);
+  res.send(data);
+});
+
+app.listen(parseInt(process.env.PORT || '3000'), '0.0.0.0', async () => {
+  await connectDB();
+  console.log(`server is running on port ${process.env.PORT}`);
+});
